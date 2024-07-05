5a3409368d72d9fd768d29048b456d6f73fd46f8
Yemin Mai <ymai5@sheffield.ac.uk>
2024-07-05 13:01:27 +0100

    fixing bugs

--- MetricGAN_KAN.py
+++ MetricGAN_KAN.py
@@ -147,66 +147,66 @@ class MetricDiscriminator(nn.Module):
         self,
         kernel_size=(5, 5),
         base_channels=15,
-        activation=nn.LeakyReLU,
+        # activation=nn.LeakyReLU,
     ):
         super().__init__()
 
-        self.activation = activation(negative_slope=0.3)
+        # self.activation = activation(negative_slope=0.3)
 
         self.BN = nn.BatchNorm2d(num_features=2, momentum=0.01)
 
         # Original implementation
 
-        self.conv1 = xavier_init_layer(
-            2, base_channels, layer_type=nn.Conv2d, kernel_size=kernel_size
-        )
-        self.conv2 = xavier_init_layer(
-            base_channels, layer_type=nn.Conv2d, kernel_size=kernel_size
-        )
-        self.conv3 = xavier_init_layer(
-            base_channels, layer_type=nn.Conv2d, kernel_size=kernel_size
-        )
-        self.conv4 = xavier_init_layer(
-            base_channels, layer_type=nn.Conv2d, kernel_size=kernel_size
-        )
-        self.Linear1 = xavier_init_layer(base_channels, out_size=50)
-        self.Linear2 = xavier_init_layer(in_size=50, out_size=10)
-        self.Linear3 = xavier_init_layer(in_size=10, out_size=1)
+        # self.conv1 = xavier_init_layer(
+        #     2, base_channels, layer_type=nn.Conv2d, kernel_size=kernel_size
+        # )
+        # self.conv2 = xavier_init_layer(
+        #     base_channels, layer_type=nn.Conv2d, kernel_size=kernel_size
+        # )
+        # self.conv3 = xavier_init_layer(
+        #     base_channels, layer_type=nn.Conv2d, kernel_size=kernel_size
+        # )
+        # self.conv4 = xavier_init_layer(
+        #     base_channels, layer_type=nn.Conv2d, kernel_size=kernel_size
+        # )
+        # self.Linear1 = xavier_init_layer(base_channels, out_size=50)
+        # self.Linear2 = xavier_init_layer(in_size=50, out_size=10)
+        # self.Linear3 = xavier_init_layer(in_size=10, out_size=1)
 
         # Modifications
 
-        #self.conv1 = KAN_Convolutional_Layer(n_convs=base_channels, kernel_size=kernel_size, device=device)
-        #self.conv2 = KAN_Convolutional_Layer(n_convs=base_channels, kernel_size=kernel_size, device=device)
-        # self.conv3 = KAN_Convolutional_Layer(n_convs=base_channels, kernel_size=kernel_size)
-        # self.conv4 = KAN_Convolutional_Layer(n_convs=base_channels, kernel_size=kernel_size)
+        self.conv1 = KAN_Convolutional_Layer(n_convs=base_channels, kernel_size=kernel_size, device=device)
+        self.conv2 = KAN_Convolutional_Layer(n_convs=base_channels, kernel_size=kernel_size, device=device)
+        self.conv3 = KAN_Convolutional_Layer(n_convs=base_channels, kernel_size=kernel_size, device=device)
+        self.conv4 = KAN_Convolutional_Layer(n_convs=base_channels, kernel_size=kernel_size, device=device)
 
-        #self.Linear1 = KANLinear(base_channels, out_features=16)
-        #self.Linear2 = KANLinear(in_features=16, out_features=1)
-        # self.Linear3 = KANLinear(base_channels, out_features=1)
+        self.Linear1 = KANLinear(base_channels, out_features=50)
+        self.Linear2 = KANLinear(in_features=50, out_features=10)
+        self.Linear3 = KANLinear(in_features=10, out_features=1)
 
     def forward(self, x):
         """Processes the input tensor x and returns an output tensor."""
         out = self.BN(x)
 
         out = self.conv1(out)
-        out = self.activation(out)
+        # out = self.activation(out)
 
         out = self.conv2(out)
-        out = self.activation(out)
+        # out = self.activation(out)
 
         out = self.conv3(out)
-        out = self.activation(out)
+        # out = self.activation(out)
 
         out = self.conv4(out)
-        out = self.activation(out)
+        # out = self.activation(out)
 
         out = torch.mean(out, (2, 3))
 
         out = self.Linear1(out)
-        out = self.activation(out)
+        # out = self.activation(out)
 
         out = self.Linear2(out)
-        out = self.activation(out)
+        # out = self.activation(out)
 
         out = self.Linear3(out)
 
