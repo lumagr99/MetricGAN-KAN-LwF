0867e8262ae1c4ead81bddc07e3f154d075c1204
Yemin Mai <ymai5@sheffield.ac.uk>
2024-07-05 01:46:17 +0100

    fix bugs

--- train.py
+++ train.py
@@ -30,6 +30,8 @@ from speechbrain.processing.features import spectral_magnitude
 from speechbrain.utils.distributed import run_on_main
 from speechbrain.utils.metric_stats import MetricStats
 
+from MetricGAN_KAN import MetricDiscriminator
+
 
 def pesq_eval(pred_wav, target_wav):
     """Normalized PESQ (to 0-1)"""
@@ -571,6 +573,10 @@ if __name__ == "__main__":
     hparams_file, run_opts, overrides = sb.parse_arguments(sys.argv[1:])
     with open(hparams_file) as fin:
         hparams = load_hyperpyyaml(fin, overrides)
+
+    # Create the model
+    hparams.models.generator = speechbrain.lobes.models.MetricGAN.EnhancementGenerator()
+    hparams.models.discriminator = MetricDiscriminator(hparams.models.kernel_size, hparams.models.base_channels)
 
     # Initialize ddp (useful only for multi-GPU DDP training)
     sb.utils.distributed.ddp_init_group(run_opts)
