77ac9a781632fea1033100635c920af7db6b834b
Yemin Mai <ymai5@sheffield.ac.uk>
2024-07-28 04:39:50 +0100

    gru-kan

--- models/MetricGAN_KAN.py
+++ models/MetricGAN_KAN.py
@@ -136,8 +136,8 @@ class EnhancementGenerator(nn.Module):
 
         for i in range(seq_lengths):
             for j in range(1, self.num_layers + 1):
-                ht_f[j, :, :] = self.gru_cell_f[j - 1](x[:, i, :], ht_f[j - 1, :, :]).clone()
-                ht_b[j, :, :] = self.gru_cell_b[j - 1](x[:, -1 - i, :], ht_b[j - 1, :, :]).clone()
+                ht_f[j, :, :] = self.gru_cell_f[j - 1](x[:, i, :], ht_f[j - 1, :, :].clone())
+                ht_b[j, :, :] = self.gru_cell_b[j - 1](x[:, -1 - i, :], ht_b[j - 1, :, :].clone())
             out[:, i, :] = self.linear(ht[-1, :, :])
 
         out = self.Learnable_sigmoid(out)
