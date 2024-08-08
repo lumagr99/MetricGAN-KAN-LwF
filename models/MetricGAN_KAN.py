ce3e3011309ee12e244afc0baddeb71e533c524c
Yemin Mai <ymai5@sheffield.ac.uk>
2024-08-08 22:08:02 +0100

    mgk-g5-d1

--- models/MetricGAN_KAN.py
+++ models/MetricGAN_KAN.py
@@ -10,7 +10,8 @@ from torch import nn
 from torch.nn.utils import spectral_norm
 
 from efficient_kan import KANLinear
-from kan_convolutional.KANConv import KAN_Convolutional_Layer
+# from kan_convolutional.KANConv import KAN_Convolutional_Layer
+from kan_convs import KANConv2DLayer
 
 import speechbrain as sb
 
@@ -116,46 +117,20 @@ class EnhancementGenerator(nn.Module):
             *(nn.GRUCell(hidden_size, hidden_size, device=device) for _ in range(self.num_layers - 1))
             ])
         
+        self.gru_linear = KANLinear(hidden_size * 2, hidden_size * 2)
         self.linear = KANLinear(hidden_size * 2, 257)
 
-        # self.linear1 = xavier_init_layer(400, 300, spec_norm=False)
-        # self.linear2 = xavier_init_layer(300, 257, spec_norm=False)
-
-        # self.linear1 = KANLinear(400, 80)
-        # self.linear2 = xavier_init_layer(80, 257, spec_norm=False)
-
         self.Learnable_sigmoid = Learnable_sigmoid()
-        # self.sigmoid = nn.Sigmoid()
 
     def forward(self, x: torch.Tensor, lengths):
         """Processes the input tensor x and returns an output tensor."""
         batch_size = x.size(0)
         seq_lengths = x.size(1)
 
-        # ht = torch.zeros(self.num_layers + 1, batch_size, self.hidden_size * 2, device=device)
         ht_f = [torch.zeros(batch_size, self.hidden_size, device=device), *(None for _ in range(self.num_layers - 1))]
         ht_b = [torch.zeros(batch_size, self.hidden_size, device=device), *(None for _ in range(self.num_layers - 1))]
-        # ht_f, ht_b  = ht.chunk(2, 2)
 
         out = torch.zeros(batch_size, seq_lengths, 257, device=device)
-
-    
-        # ht_f, ht_b = ht[0].chunk(2, 1)
-        # for j in range(seq_lengths):
-        #     ht_f = self.gru_cell_f[0](x[:, j, :], ht_f)
-        #     ht_b = self.gru_cell_b[0](x[:, -1 - j, :], ht_b)
-        # for i in range(1, self.num_layers):
-        #     ht_f, ht_b = ht[i].chunk(2, 1)
-        #     ht_f_0, ht_b_0 = ht[i - 1].chunk(2, 1)
-        #     for j in range(seq_lengths):
-        #         ht_f = self.gru_cell_f[i - 1](ht_f_0, ht_f)
-        #         ht_b = self.gru_cell_b[i - 1](ht_b_0, ht_b)
-
-        # for j in range(seq_lengths):
-        #     ht_f = self.gru_cell_f[0](x[:, j, :], ht_f)
-        #     ht_b = self.gru_cell_b[0](x[:, -1 - j, :], ht_b)
-        # out[:, i, :] = self.linear(ht[-1])
-            
 
         for i in range(seq_lengths):
             ht_f[0] = self.gru_cell_f[0](x[:, i, :], ht_f[0])
@@ -163,8 +138,9 @@ class EnhancementGenerator(nn.Module):
             for j in range(1, self.num_layers):
                 ht_f[j] = self.gru_cell_f[j](ht_f[j - 1], ht_f[j])
                 ht_b[j] = self.gru_cell_b[j](ht_b[j - 1], ht_b[j])
-            out[:, i, :] = self.linear(torch.concat((ht_f[-1], ht_b[-1]), 1))
+            out[:, i, :] = self.gru_linear(torch.concat((ht_f[-1], ht_b[-1]), 1))
 
+        out = self.linear(out)
         out = self.Learnable_sigmoid(out)
 
         return out
@@ -192,69 +168,30 @@ class MetricDiscriminator(nn.Module):
         self,
         kernel_size=(5, 5),
         base_channels=15,
-        activation=nn.LeakyReLU,
+        # activation=nn.LeakyReLU,
     ):
         super().__init__()
 
-        self.activation = activation(negative_slope=0.3)
+        # self.activation = activation(negative_slope=0.3)
 
         self.BN = nn.BatchNorm2d(num_features=2, momentum=0.01)
 
-        # Original implementation
-
-        self.conv1 = xavier_init_layer(
-            2, base_channels, layer_type=nn.Conv2d, kernel_size=kernel_size
-        )
-        self.conv2 = xavier_init_layer(
-            base_channels, layer_type=nn.Conv2d, kernel_size=kernel_size
-        )
-        self.conv3 = xavier_init_layer(
-            base_channels, layer_type=nn.Conv2d, kernel_size=kernel_size
-        )
-        self.conv4 = xavier_init_layer(
-            base_channels, layer_type=nn.Conv2d, kernel_size=kernel_size
-        )
-        self.Linear1 = xavier_init_layer(base_channels, out_size=50)
-        self.Linear2 = xavier_init_layer(in_size=50, out_size=10)
-        self.Linear3 = xavier_init_layer(in_size=10, out_size=1)
-
         # Modifications
 
-        # self.conv1 = KAN_Convolutional_Layer(n_convs=base_channels, kernel_size=kernel_size, device=device)
-        # self.conv2 = KAN_Convolutional_Layer(n_convs=base_channels, kernel_size=kernel_size, device=device)
-        # self.conv1 = KAN_Convolutional_Layer(n_convs=base_channels, kernel_size=(9, 9), device=device)
-        # self.conv2 = KAN_Convolutional_Layer(n_convs=base_channels, kernel_size=(3, 3), device=device)
-        # self.conv3 = KAN_Convolutional_Layer(n_convs=base_channels, kernel_size=kernel_size, device=device)
-        # self.conv4 = KAN_Convolutional_Layer(n_convs=base_channels, kernel_size=kernel_size, device=device)
+        self.conv1 = KANConv2DLayer(2, base_channels, kernel_size, base_activation=nn.SiLU)
+        self.conv2 = KANConv2DLayer(base_channels, base_channels, kernel_size, base_activation=nn.SiLU)
 
-        # self.Linear1 = KANLinear(in_features=2*base_channels*base_channels, out_features=1)
-        # self.Linear2 = KANLinear(in_features=50, out_features=1)
-        # self.Linear3 = KANLinear(in_features=10, out_features=1)
+        self.Linear1 = KANLinear(in_features=base_channels, out_features=1)
 
     def forward(self, x):
         """Processes the input tensor x and returns an output tensor."""
         out = self.BN(x)
 
         out = self.conv1(out)
-        out = self.activation(out)
-
         out = self.conv2(out)
-        out = self.activation(out)
-
-        out = self.conv3(out)
-        out = self.activation(out)
-
-        out = self.conv4(out)
-        out = self.activation(out)
 
         out = torch.mean(out, (2, 3))
 
         out = self.Linear1(out)
-        out = self.activation(out)
-
-        out = self.Linear2(out)
-        out = self.activation(out)
-
-        out = self.Linear3(out)
 
         return out
